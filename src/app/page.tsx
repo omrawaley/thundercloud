13de1ac0ee1f9a8436119abb586e869c4fcdc170
omrawaley <omrawaley@icloud.com>
2024-12-23 17:59:36 -0500

    added world map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+// 'use client'
+
 import Navbar from "@/components/Navbar";
 import Section from "@/components/Section";
+import { Typography } from "@/components/Typography";
+import WorldMap from "@/components/ui/world-map";
 
 function Home() {
   return (
@@ -23,9 +27,49 @@ function Home() {
             dir="left"
             />
 
+            <div className="flex flex-col bg-gradient-to-r from-red to-magenta p-20 mt-10 rounded-2xl text-center">
+                <Typography variant="h1" className="font-brand text-white ml-2">Enjoy Servers Across the Globe</Typography>
+                <Typography variant="p" className="font-brand text-white mr-2 mb-10">Nodes, or servers, can be hosted from anywhere by users, ensuring blazing fast data transfers and secure data fragmentation.</Typography>
+                <WorldMap
+                    lineColor="#ff2222"
+                    dots={[
+                      {
+                        start: {
+                          lat: 64.2008,
+                          lng: -149.4937,
+                        }, // Alaska (Fairbanks)
+                        end: {
+                          lat: 34.0522,
+                          lng: -118.2437,
+                        }, // Los Angeles
+                      },
+                      {
+                        start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
+                        end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+                      },
+                      {
+                        start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+                        end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
+                      },
+                      {
+                        start: { lat: 51.5074, lng: -0.1278 }, // London
+                        end: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                      },
+                      {
+                        start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                        end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
+                      },
+                      {
+                        start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                        end: { lat: -1.2921, lng: 36.8219 }, // Nairobi
+                      },
+                    ]}
+                  />
+            </div>
+
         <Section
             title="Make Big Profit"
-            body="Earn profit by reusing your old storage devices and contributing the space to the network as a Node™."
+            body="Earn profit by reusing your old storage devices and contributing the space to the network as a Node."
             button="Guide"
             image="/NetworkImage.png"
             dir="right"
@@ -38,8 +82,6 @@ function Home() {
             image="/NetworkImage.png"
             dir="left"
             />
-
-
         </div>
     </div>
   );
